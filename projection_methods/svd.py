9bd5d29be91b3f46a32e6547a92d4bc4313fae23
Apoorv Walia <apoorv96@gmail.com>
2024-03-18 04:48:40 -0500

    Removed some comments

--- projection_methods/svd.py
+++ projection_methods/svd.py
@@ -8,25 +8,20 @@ class SVDApproximator(approximator.Approximator):
     def approximate(self, optim_mat: np.ndarray, nb_params_share: float = 0.03125):
         assert len(optim_mat.shape) == 2, "Can only handle matrices for LR approximation"
 
-        print("checkpoint 1")
         max_nb_parameters = int(nb_params_share * optim_mat.size)
         rank = int(max_nb_parameters / (optim_mat.shape[0] + optim_mat.shape[1]))
 
-        print("checkpoint 2")
         U, S, Vh = np.linalg.svd(optim_mat, full_matrices=False)
         S_root = np.sqrt(S)
         left_full_component = U @ np.diag(S_root)
         right_full_component = np.diag(S_root) @ Vh
 
-        print("checkpoint 3")
         left_mat = left_full_component[:, :rank]
         right_mat = right_full_component[:rank, :]
 
-        print("checkpoint 4")
         approx_mat_dense = left_mat @ right_mat
         nb_parameters = left_mat.size + right_mat.size
 
-        print("checkpoint 5")
         res_dict = dict()
         res_dict["type"] = "LRApproximator"
         res_dict["left_mat"] = left_mat
